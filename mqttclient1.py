a8b819191c0dc940a32ad34465867a6a796c2469
Daniel Asus Laptop <daniel.schoonwinkel@gmail.com>
2020-04-01 10:48:31 +0200

    MQTT client successfully pulling inverter data

--- mqttclient1.py
+++ mqttclient1.py
@@ -2,8 +2,9 @@
 import paho.mqtt.client as mqtt
 import time
 import csv
+import configparser
 
-fieldnames = ["epochTime", "GridWatts"]
+fieldnames = ["epochTime", "topic", "value"]
 
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
@@ -13,7 +14,8 @@ def on_message(client, userdata, message):
 
         info = {
             "epochTime": epoch_count,
-            "GridWatts": str(message.payload.decode("utf-8"))
+            "topic" : message.topic,
+            "value": str(message.payload.decode("utf-8"))
         }
         print(info)
         csv_writer.writerow(info)
@@ -22,22 +24,29 @@ def on_message(client, userdata, message):
     # print("message qos=",message.qos)
     # print("message retain flag=",message.retain)
 
-with open('data.csv', 'w') as csv_file:
-    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    csv_writer.writeheader()
+# with open('data.csv', 'w') as csv_file:
+#     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+#     csv_writer.writeheader()
 
-host = "192.168.1.52"
+config = configparser.ConfigParser()
+config.read('credentials.conf')
+username = config['credentials']['username']
+password = config['credentials']['password']
+host = config['credentials']['host']
+
+print(username)
+print(password)
+print(host)
+
 client = mqtt.Client("receiver")
 client.on_message=on_message #attach function to callback
 print("connecting to broker")
-client.username_pw_set("daniel", password="12312412412312")
+client.username_pw_set(username, password=password)
 client.connect(host) #connect to broker
 client.loop_start() #start the loop
 print("Subscribing to topic ")
 # client.subscribe("Inverter/GridWatts")
 client.subscribe("Inverter/LoadWatts")
-# print("Publishing message to topic","house/bulbs/bulb1")
-# client.publish("house/bulbs/bulb1","OFF")
 try:
     while(True):
         pass
